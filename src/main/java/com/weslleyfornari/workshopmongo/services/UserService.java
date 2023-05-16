10f36d168d321d7e2848151cb410218f8eef5d84
Weslley Fornari <weslleyfornari@gmail.com>
2023-05-15 21:36:29 -0300

    Atualização do User com PUT

--- src/main/java/com/weslleyfornari/workshopmongo/services/UserService.java
+++ src/main/java/com/weslleyfornari/workshopmongo/services/UserService.java
@@ -25,14 +25,31 @@ public class UserService {
 	public User findById(String id) {
 		
 		Optional<User> obj = repo.findById(id);
-		
-			return obj.orElseThrow(() -> new ObjectNotFoundException("Usuario não encontrado"));
-		}
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuario não encontrado"));
+	}    
+    
 
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User oldObj = findById(obj.getId());
+		updateData(oldObj, obj);
+		return repo.save(oldObj);
+		}
+
+	
+	private void updateData(User oldObj, User obj) {
+		oldObj.setName(obj.getName());
+		oldObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
